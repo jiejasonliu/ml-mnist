fac73821bddf68312f01851f4c1fcab184fe8e00
jiejasonliu <jasonningliu@gmail.com>
2020-08-19 19:25:35 -0600

    Add CNTK helper functions and create network pre-requisites

---
+++ MNISTSolution/MNIST/CNTKHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using CNTK;
+
+namespace MNIST {
+    /// <summary>
+    /// Helper functions for CNTK to create neural networks.
+    /// </summary>
+    public static class CNTKHelper {
+
+        /// <summary>
+        /// Adds a new dense layer provided node count and activation function.
+        /// </summary>
+        /// <returns></returns>
+        public static Variable AddLayer(this Variable input, int nodeCount, Func<CNTK.Variable, CNTK.Function> activationFunc, string layerName) {
+            return (Variable) activationFunc(AddLayer(input, nodeCount, layerName));
+        }
+
+        private static Variable AddLayer(this Variable input, int nodeCount, string layerName) {
+            // get compute from gpu or cpu
+            var computeDevice = GetCurrentDevice();
+
+            // create times parameter
+            var timesParameter = new Parameter(
+                shape: NDShape.CreateNDShape(new int[] { nodeCount, CNTK.NDShape.InferredDimension }),
+                dataType: DataType.Float,
+                initializer: CNTKLib.GlorotUniformInitializer(
+                    scale: CNTKLib.DefaultParamInitScale,
+                    outputRank: CNTKLib.SentinelValueForInferParamInitRank,
+                    filterRank: CNTKLib.SentinelValueForInferParamInitRank, 
+                    seed: 1
+                ),
+                device: computeDevice,
+                name: "timesParameter_" + layerName
+            );
+
+            // create times function
+            var timesFunc = CNTKLib.Times(
+                leftOperand: timesParameter,
+                rightOperand: input,
+                outputRank: 1,
+                inferInputRankToMap: 0
+            );
+
+            // create plus (new node) parameter
+            var plusParameter = new Parameter(
+                shape: CNTK.NDShape.CreateNDShape(new int[] { CNTK.NDShape.InferredDimension }),
+                initValue: 0.0F,
+                device: computeDevice,
+                "plusParameter_" + layerName);
+
+            return CNTKLib.Plus(plusParameter, timesFunc, layerName);
+        }
+
+        /// <summary>
+        /// Returns the first compatible compute GPU. CPU failsafe if no valid GPU found.
+        /// </summary>
+        public static DeviceDescriptor GetCurrentDevice() {
+            // look through all devices
+            foreach (var device in CNTK.DeviceDescriptor.AllDevices())
+                // found gpu
+                if (device.Type == CNTK.DeviceKind.GPU)
+                    return device;
+            
+            // did not find gpu, default to cpu
+            return DeviceDescriptor.CPUDevice;
+        }
+    }
+}
--- MNISTSolution/MNIST/Program.cs
+++ MNISTSolution/MNIST/Program.cs
@@ -1,9 +1,81 @@
-﻿using System;
+﻿using CNTK;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace MNIST {
     class Program {
+
+        // dataset path
+        private static readonly string TRAIN_DATA_PATH = Path.Combine(Environment.CurrentDirectory, "mnist_train.csv");
+        private static readonly string TEST_DATA_PATH = Path.Combine(Environment.CurrentDirectory, "mnist_test.csv");
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            // MLContext init
+            var mlContext = new MLContext();
+
+            // define structure of data (from csv)
+            var columnDefinition = new TextLoader.Column[] {
+                new TextLoader.Column(nameof(HandwrittenDigit.Number), DataKind.Single, 0),
+                new TextLoader.Column(nameof(HandwrittenDigit.PixelData), DataKind.Single, 1, 784)
+            };
+
+            // load IEnumerable<HandwrittenDigit> data
+            var train = LoadData(mlContext, TRAIN_DATA_PATH, columnDefinition);
+            var test = LoadData(mlContext, TEST_DATA_PATH, columnDefinition);
+
+            // map to new enumerable (LINQ)
+            var trainData = train.Select(data => data.GetFeatures()).ToArray();
+            var trainLabels = train.Select(data => data.GetLabel()).ToArray();
+            var testData = test.Select(data => data.GetFeatures()).ToArray();
+            var testLabels = test.Select(data => data.GetLabel()).ToArray();
+
+            // build features (CNTK)
+            var features = Variable.InputVariable(
+                shape: NDShape.CreateNDShape(new int[] { 28, 28 }),
+                dataType: DataType.Float
+            );
+
+            // build labels (CNTK)
+            var labels = Variable.InputVariable(
+                shape: NDShape.CreateNDShape(new int[] { 10 }),
+                dataType: DataType.Float
+            );
+
+            // build the neural network 
+            var network = BuildNetwork(features);
+
+        }
+
+        /// <summary>
+        /// Creates an IEnumerable of HandwrittenDigit(s) when provided an MLContext, data path, and the column descriptions.
+        /// </summary>
+        private static IEnumerable<HandwrittenDigit> LoadData(MLContext context, string dataPath, TextLoader.Column[] columnDefinition) {
+            // create IDataView
+            var dataView = context.Data.LoadFromTextFile(
+                path: dataPath,
+                columns: columnDefinition,
+                hasHeader: true,
+                separatorChar: ','
+            );
+
+            // create dataset
+            var dataset = context.Data.CreateEnumerable<HandwrittenDigit>(
+                data: dataView,
+                reuseRowObject: false
+            );
+
+            return dataset;
+        }
+
+        /// <summary>
+        /// Builds the network with ReLU input (standard) and Softmax output (mutually exclusive labels).
+        /// </summary>
+        private static Variable BuildNetwork(Variable input) {
+            return null;
         }
     }
 }
