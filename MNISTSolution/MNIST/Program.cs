d338ad7a4c11c8bed84cedcc4d721e433738937b
jiejasonliu <jasonningliu@gmail.com>
2020-08-20 00:03:56 -0600

    Load transformer if existing trained model

--- MNISTSolution/MNIST/Program.cs
+++ MNISTSolution/MNIST/Program.cs
@@ -15,6 +15,7 @@ namespace MNIST {
 
         // saved schema (model) path
         private static readonly string SCHEMA_MODEL_PATH = Path.Combine("schema", "model.zip");
+        private static readonly string ENV_SCHEMA_MODEL_PATH = Path.Combine(Environment.CurrentDirectory, SCHEMA_MODEL_PATH);
 
         // feature column default name
         private static readonly string DEFAULT_FEATURE_NAME = "Features";
@@ -23,67 +24,78 @@ namespace MNIST {
             // MLContext init
             var mlContext = new MLContext();
 
-            // define structure of data (from csv)
-            var columnDefinition = new[] {
-                new TextLoader.Column(nameof(HandwrittenDigit.PixelData), DataKind.Single, 1, 784),
-                new TextLoader.Column(nameof(HandwrittenDigit.Number), DataKind.Single, 0)
-            };
+            // neural network declaration
+            ITransformer model;
 
-            // load IEnumerable<HandwrittenDigit> data
-            var train = LoadDataView(mlContext, TRAIN_DATA_PATH, columnDefinition);
-            var test = LoadDataView(mlContext, TEST_DATA_PATH, columnDefinition);
+            // try to load existing model
+            if (File.Exists(ENV_SCHEMA_MODEL_PATH)) {
+                Console.WriteLine("Found existing model at: " + ENV_SCHEMA_MODEL_PATH);
+                DataViewSchema schema;
+                model = mlContext.Model.Load(SCHEMA_MODEL_PATH, out schema);
+            }
+            // train model normally
+            else {
+                // define structure of data (from csv)
+                var columnDefinition = new[] {
+                    new TextLoader.Column(nameof(HandwrittenDigit.PixelData), DataKind.Single, 1, 784),
+                    new TextLoader.Column(nameof(HandwrittenDigit.Number), DataKind.Single, 0)
+                };
 
-            // build the training pipeline
-            // Stochastic Dual Coordinate Ascent algorithm
+                // create IDataView object
+                var train = LoadDataView(mlContext, TRAIN_DATA_PATH, columnDefinition);
+                var test = LoadDataView(mlContext, TEST_DATA_PATH, columnDefinition);
 
-            // create pipeline
-            // merge PixelData columns (784) into one specialized Feature column
-            var pipeline = mlContext.Transforms.Concatenate(
-                outputColumnName: DEFAULT_FEATURE_NAME, 
-                inputColumnNames: nameof(HandwrittenDigit.PixelData))
+                // build the training pipeline
+                // merge PixelData columns (784) into one specialized Feature column
+                var pipeline = mlContext.Transforms.Concatenate(
+                    outputColumnName: DEFAULT_FEATURE_NAME,
+                    inputColumnNames: nameof(HandwrittenDigit.PixelData))
 
-                // convert number column to keys
-                .Append(mlContext.Transforms.Conversion.MapValueToKey("Number", nameof(HandwrittenDigit.Number)))
+                    // convert number column to keys
+                    .Append(mlContext.Transforms.Conversion.MapValueToKey("Number", nameof(HandwrittenDigit.Number)))
 
-                // cache data for faster training
-                .AppendCacheCheckpoint(mlContext)
+                    // cache data for faster training
+                    .AppendCacheCheckpoint(mlContext)
 
-                // train using SDCA algorithm
-                .Append(mlContext.MulticlassClassification.Trainers.SdcaNonCalibrated(
-                    labelColumnName: nameof(HandwrittenDigit.Number),
-                    featureColumnName: DEFAULT_FEATURE_NAME
-                )
-            );
+                    // train using SDCA algorithm
+                    .Append(mlContext.MulticlassClassification.Trainers.SdcaNonCalibrated(
+                        labelColumnName: nameof(HandwrittenDigit.Number),
+                        featureColumnName: DEFAULT_FEATURE_NAME
+                    )
+                );
 
-            // train the model
-            Console.WriteLine("Training model...");
+                // train the model
+                Console.WriteLine("Training model...");
 
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            var model = pipeline.Fit(train);
-            watch.Stop();
-            Console.WriteLine($"Model took {(watch.ElapsedMilliseconds / 1000.0):#.###} seconds");
-            Console.WriteLine();
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                model = pipeline.Fit(train);
+                watch.Stop();
+                Console.WriteLine($"Model took {(watch.ElapsedMilliseconds / 1000.0):#.###} seconds");
+                Console.WriteLine();
 
-            // save model locally
-            mlContext.Model.Save(model, train.Schema, SCHEMA_MODEL_PATH);
+                // save model locally
+                mlContext.Model.Save(model, train.Schema, SCHEMA_MODEL_PATH);
+                Console.WriteLine("Model saved at: " + ENV_SCHEMA_MODEL_PATH));
+                Console.WriteLine();
 
-            // make predictions on test data
-            Console.WriteLine("Evaluating model...");
-            var predictions = model.Transform(test);
+                // make predictions on test data
+                Console.WriteLine("Evaluating model...");
+                var predictions = model.Transform(test);
 
-            // evaluate predictions
-            var metrics = mlContext.MulticlassClassification.Evaluate(
-                data: predictions,
-                labelColumnName: "Number"
-            );
+                // evaluate predictions
+                var metrics = mlContext.MulticlassClassification.Evaluate(
+                    data: predictions,
+                    labelColumnName: "Number"
+                );
 
-            // show evaluation metrics
-            Console.WriteLine($"Evaluation metrics: ");
-            Console.WriteLine($"MicroAccuracy:    {metrics.MicroAccuracy:0.###}");
-            Console.WriteLine($"MacroAccuracy:    {metrics.MacroAccuracy:0.###}");
-            Console.WriteLine($"LogLoss:          {metrics.LogLoss:#.###}");
-            Console.WriteLine($"LogLossReduction: {metrics.LogLossReduction:#.###}");
-            Console.WriteLine();
+                // show evaluation metrics
+                Console.WriteLine($"Evaluation metrics: ");
+                Console.WriteLine($" MicroAccuracy:    {metrics.MicroAccuracy:0.###}");
+                Console.WriteLine($" MacroAccuracy:    {metrics.MacroAccuracy:0.###}");
+                Console.WriteLine($" LogLoss:          {metrics.LogLoss:#.###}");
+                Console.WriteLine($" LogLossReduction: {metrics.LogLossReduction:#.###}");
+                Console.ReadLine();
+            }
         }
 
         /// <summary>
