1d0aa82d1c83f61a1dd430be6b8abd06ac08f967
jiejasonliu <jasonningliu@gmail.com>
2020-08-19 23:30:39 -0600

    Drop CNTK and implement neural network using SDCA pipeline

--- MNISTSolution/MNIST/HandwrittenDigit.cs
+++ MNISTSolution/MNIST/HandwrittenDigit.cs
@@ -15,32 +15,14 @@ namespace MNIST {
     /// where the label (digit) is mutually exclusive and can only be (0...9).
     /// </summary>
     class HandwrittenDigit {
-
         // use float type to minimize memory cost //
 
-        [LoadColumn(0)]
+        [ColumnName("Number"), LoadColumn(0)]
         public float Number = default;
 
+        [ColumnName("PixelData"), LoadColumn(1, 784)]
         [VectorType(784)]
         public float[] PixelData = default;
-
-        // GET: feature(s)
-        public float[] GetFeatures() => PixelData;
-
-        // GET: label
-        // 10-dimension vector with binary results (1.0 or 0.0) if neuron is active
-        public float[] GetLabel() => new float[] {
-            Number == 0 ? 1.0F : 0.0F,
-            Number == 1 ? 1.0F : 0.0F,
-            Number == 2 ? 1.0F : 0.0F,
-            Number == 3 ? 1.0F : 0.0F,
-            Number == 4 ? 1.0F : 0.0F,
-            Number == 5 ? 1.0F : 0.0F,
-            Number == 6 ? 1.0F : 0.0F,
-            Number == 7 ? 1.0F : 0.0F,
-            Number == 8 ? 1.0F : 0.0F,
-            Number == 9 ? 1.0F : 0.0F,
-        };
 
     }
 }
--- MNISTSolution/MNIST/Program.cs
+++ MNISTSolution/MNIST/Program.cs
@@ -13,58 +13,83 @@ namespace MNIST {
         private static readonly string TRAIN_DATA_PATH = Path.Combine(Environment.CurrentDirectory, "mnist_train.csv");
         private static readonly string TEST_DATA_PATH = Path.Combine(Environment.CurrentDirectory, "mnist_test.csv");
 
+        // saved schema (model) path
+        private static readonly string SCHEMA_MODEL_PATH = Path.Combine(Environment.CurrentDirectory, "/schema/model.zip");
+
+        // feature column default name
+        private static readonly string DEFAULT_FEATURE_NAME = "Features";
+
         static void Main(string[] args) {
             // MLContext init
             var mlContext = new MLContext();
 
             // define structure of data (from csv)
-            var columnDefinition = new TextLoader.Column[] {
-                new TextLoader.Column(nameof(HandwrittenDigit.Number), DataKind.Single, 0),
-                new TextLoader.Column(nameof(HandwrittenDigit.PixelData), DataKind.Single, 1, 784)
+            var columnDefinition = new[] {
+                new TextLoader.Column(nameof(HandwrittenDigit.PixelData), DataKind.Single, 1, 784),
+                new TextLoader.Column(nameof(HandwrittenDigit.Number), DataKind.Single, 0)
             };
 
             // load IEnumerable<HandwrittenDigit> data
-            var train = LoadData(mlContext, TRAIN_DATA_PATH, columnDefinition);
-            var test = LoadData(mlContext, TEST_DATA_PATH, columnDefinition);
+            var train = LoadDataView(mlContext, TRAIN_DATA_PATH, columnDefinition);
+            var test = LoadDataView(mlContext, TEST_DATA_PATH, columnDefinition);
 
-            // map to new enumerable (LINQ)
-            var trainData = train.Select(data => data.GetFeatures()).ToArray();
-            var trainLabels = train.Select(data => data.GetLabel()).ToArray();
-            var testData = test.Select(data => data.GetFeatures()).ToArray();
-            var testLabels = test.Select(data => data.GetLabel()).ToArray();
+            // build the training pipeline
+            // Stochastic Dual Coordinate Ascent algorithm
 
-            // build features (CNTK)
-            var features = Variable.InputVariable(
-                shape: NDShape.CreateNDShape(new int[] { 28, 28 }),
-                dataType: DataType.Float
+            // create pipeline
+            // merge PixelData columns (784) into one specialized Feature column
+            var pipeline = mlContext.Transforms.Concatenate(
+                outputColumnName: DEFAULT_FEATURE_NAME, 
+                inputColumnNames: nameof(HandwrittenDigit.PixelData))
+
+                // convert number column to keys
+                .Append(mlContext.Transforms.Conversion.MapValueToKey("Number", nameof(HandwrittenDigit.Number)))
+
+                // cache data for faster training
+                .AppendCacheCheckpoint(mlContext)
+
+                // train using SDCA algorithm
+                .Append(mlContext.MulticlassClassification.Trainers.SdcaNonCalibrated(
+                    labelColumnName: nameof(HandwrittenDigit.Number),
+                    featureColumnName: DEFAULT_FEATURE_NAME
+                )
             );
 
-            // build labels (CNTK)
-            var labels = Variable.InputVariable(
-                shape: NDShape.CreateNDShape(new int[] { 10 }),
-                dataType: DataType.Float
+            // train the model
+            Console.WriteLine("Training model...");
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var model = pipeline.Fit(train);
+            watch.Stop();
+            Console.WriteLine($"Model took {(watch.ElapsedMilliseconds / 1000.0):#.###} seconds");
+            Console.WriteLine();
+
+            // make predictions on test data
+            Console.WriteLine("Evaluating model...");
+            var predictions = model.Transform(test);
+
+            // evaluate predictions
+            var metrics = mlContext.MulticlassClassification.Evaluate(
+                data: predictions,
+                labelColumnName: "Number"
             );
 
-            // build the neural network 
-            var network = BuildNetwork(features);
-
+            // show evaluation metrics
+            Console.WriteLine($"Evaluation metrics: ");
+            Console.WriteLine($"MicroAccuracy:    {metrics.MicroAccuracy:0.###}");
+            Console.WriteLine($"MacroAccuracy:    {metrics.MacroAccuracy:0.###}");
+            Console.WriteLine($"LogLoss:          {metrics.LogLoss:#.###}");
+            Console.WriteLine($"LogLossReduction: {metrics.LogLossReduction:#.###}");
+            Console.WriteLine();
         }
 
         /// <summary>
         /// Creates an IEnumerable of HandwrittenDigit(s) when provided an MLContext, data path, and the column descriptions.
         /// </summary>
-        private static IEnumerable<HandwrittenDigit> LoadData(MLContext context, string dataPath, TextLoader.Column[] columnDefinition) {
-            // create IDataView
-            var dataView = context.Data.LoadFromTextFile(
-                path: dataPath,
-                columns: columnDefinition,
-                hasHeader: true,
-                separatorChar: ','
-            );
-
+        private static IEnumerable<HandwrittenDigit> LoadDataset(MLContext context, string dataPath, TextLoader.Column[] columnDefinition) {
             // create dataset
             var dataset = context.Data.CreateEnumerable<HandwrittenDigit>(
-                data: dataView,
+                data: LoadDataView(context, dataPath, columnDefinition),
                 reuseRowObject: false
             );
 
@@ -72,10 +97,18 @@ namespace MNIST {
         }
 
         /// <summary>
-        /// Builds the network with ReLU input (standard) and Softmax output (mutually exclusive labels).
+        /// Creates an IDataView when provided an MLContext, data path, and the column descriptions.
         /// </summary>
-        private static Variable BuildNetwork(Variable input) {
-            return null;
+        private static IDataView LoadDataView(MLContext context, string dataPath, TextLoader.Column[] columnDefinition) {
+            // create IDataView
+            var dataView = context.Data.LoadFromTextFile(
+                path: dataPath,
+                columns: columnDefinition,
+                hasHeader: false,
+                separatorChar: ','
+            );
+
+            return dataView;
         }
     }
 }
